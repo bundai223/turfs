53541e6459cfbd3facca09d30fe5e5d00f0a501e
bundai223 <bundai223@gmail.com>
2018-07-27 01:53:05 +0900

    svgで描画

--- server/app/javascript/app.vue
+++ server/app/javascript/app.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <svg viewbox="0 0 300 300" width="300" height="300" >
+      <line :x1="x1" y1=100 x2=200 y2=200 stroke="black"></line>
+    <circle r=50 cx=150 @click="active = !active" :class="{'active': active}"></circle>
+    </svg>
+    <div>
+      <input type="range" v-model="x1">
+    </div>
+
   </div>
 </template>
 
@@ -8,7 +15,8 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+x1: 100,
+active: false
     }
   }
 }
@@ -18,5 +26,16 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
+}
+
+circle {
+  fill: #333;
+  transition: all 0.4s cubic-bezier(.96, .04, .04, .96);
+  stroke: rgb(119, 0, 255);
+  storoke-width: 1px;
+}
+.active{
+    fill: white;
+    stroke-width: 50px;
 }
 </style>
