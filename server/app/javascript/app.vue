267aca6a88ab44270cfdc729254b0c028e8576ec
bundai223 <bundai223@gmail.com>
2018-07-31 01:28:12 +0900

    calendar added

--- server/app/javascript/app.vue
+++ server/app/javascript/app.vue
@@ -1,21 +1,15 @@
 <template>
   <div id="app">
-    <svg viewbox="0 0 300 300" width="300" height="300" >
-      <line :x1="x1" y1=100 x2=200 y2=200 stroke="black"></line>
-    <circle r=50 cx=150 @click="active = !active" :class="{'active': active}"></circle>
-    </svg>
-    <div>
-      <input type="range" v-model="x1">
-    </div>
-
+    <calendar />;
   </div>
 </template>
 
 <script>
+  import { Calendar } from 'quasar-calendar'
+
 export default {
   data: function () {
     return {
-x1: 100,
 active: false
     }
   }
@@ -26,16 +20,5 @@ active: false
 p {
   font-size: 2em;
   text-align: center;
-}
-
-circle {
-  fill: #333;
-  transition: all 0.4s cubic-bezier(.96, .04, .04, .96);
-  stroke: rgb(119, 0, 255);
-  storoke-width: 1px;
-}
-.active{
-    fill: white;
-    stroke-width: 50px;
 }
 </style>
